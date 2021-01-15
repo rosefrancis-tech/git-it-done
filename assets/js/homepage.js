a3fbf4acf0ac85855a029ef63b97ebaefa319f32
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-14 19:42:26 -0500

    display repos of searched user

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,10 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+
+// Function for API call
 var getUserRepos = function(user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -10,8 +17,6 @@ var getUserRepos = function(user) {
     });
   });
 };
-var userFormEl = document.querySelector("#user-form");
-var nameInputEl = document.querySelector("#username");
 
 // Function for form submission
 var formSubmitHandler = function(event) {
@@ -31,6 +36,29 @@ if (username) {
 var displayRepos = function(repos, searchTerm) {
   console.log(repos);
   console.log(searchTerm);
+  // clear old content
+  repoContainerEl.textContent = "";
+  repoSearchTerm.textContent = searchTerm;
+
+  // loop over repos
+for (var i = 0; i < repos.length; i++) {
+  // format repo name
+  var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+  // create a container for each repo
+  var repoEl = document.createElement("div");
+  repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+  // create a span element to hold repository name
+  var titleEl = document.createElement("span");
+  titleEl.textContent = repoName;
+
+  // append to container
+  repoEl.appendChild(titleEl);
+
+  // append container to the dom
+  repoContainerEl.appendChild(repoEl);
+}
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
