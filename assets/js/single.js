83f554796800f3b6e7a8638091a97c7144b21f34
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-15 16:07:12 -0500

    display link to actual issues page

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issue-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -12,8 +13,8 @@ var getRepoIssues = function(repo) {
 
                 // check if api has paginated issues
                 if (response.headers.get("Link")) {
-                console.log("repo has more than 30 issues");
-      }
+                displayWarning(repo);
+                }
             });
         }
         else {
@@ -58,4 +59,16 @@ var displayIssues = function(issues) {
 
 };
 
-getRepoIssues("facebook/react");
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+  };
+
+getRepoIssues("facebook/chisel");
