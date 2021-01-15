9b7f33b52bb85e0c1a89e9fe56fda9c4a333544a
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-15 10:58:35 -0500

    pass response data to dom function

---
+++ assets/js/single.js
@@ -0,0 +1,30 @@
+
+
+
+var getRepoIssues = function(repo) {
+    console.log(repo);
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                // pass response data to dom function
+                displayIssues(data);
+            });
+        }
+        else {
+            alert("There was a problem with your request!");
+        }
+    });
+};
+  
+var displayIssues = function(issues) {
+    for (var i = 0; i < issues.length; i++) {
+        // create a link element to take users to the issue on github
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+    }
+};
+  getRepoIssues("facebook/react");
