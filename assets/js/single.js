9e2d87f692cc90838d85441a7da376accdd3cf39
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-15 15:26:11 -0500

    display issues/pull requests in browser

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,4 @@
-
-
+var issueContainerEl = document.querySelector("#issue-container");
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -19,12 +18,39 @@ var getRepoIssues = function(repo) {
 };
   
 var displayIssues = function(issues) {
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+    }
     for (var i = 0; i < issues.length; i++) {
         // create a link element to take users to the issue on github
         var issueEl = document.createElement("a");
         issueEl.classList = "list-item flex-row justify-space-between align-center";
         issueEl.setAttribute("href", issues[i].html_url);
         issueEl.setAttribute("target", "_blank");
+        
+        // create span to hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        // append to container
+        issueEl.appendChild(titleEl);
+
+        // create a type element
+        var typeEl = document.createElement("span");
+
+        // check if issue is an actual issue or a pull request
+        if (issues[i].pull_request) {
+        typeEl.textContent = "(Pull request)";
+        } else {
+        typeEl.textContent = "(Issue)";
+        }
+        // append to container
+        issueEl.appendChild(typeEl)
+
+        issueContainerEl.appendChild(issueEl);
     }
+
 };
-  getRepoIssues("facebook/react");
+
+getRepoIssues("facebook/react");
