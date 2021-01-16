0083c02255fe9eca2fc79c34b91eacee68185779
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-15 22:47:38 -0500

    add button elements

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -92,7 +92,16 @@ var displayRepos = function(repos, searchTerm) {
 
 var getFeaturedRepos = function(language) {
   var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
-  fetch(apiUrl);
+  fetch(apiUrl).then(function(response) {
+    if(response.ok) {
+      response.json().then(function(data) {
+      displayRepos(data.items, language);  
+      });
+    }
+    else {
+      alert("Error: " + response.statusText);
+    }  
+  });
 };
 
 // add event listeners to forms
