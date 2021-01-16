daab7fd9c3ba9f269f5072885a2c9aa610beb288
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-15 22:56:16 -0500

    add clickable events

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector('#user-form');
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
+var languageButtonsEl = document.querySelector('#language-buttons');
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -104,5 +105,17 @@ var getFeaturedRepos = function(language) {
   });
 };
 
+var buttonClickHandler = function(event) {
+  var language = event.target.getAttribute("data-language");
+  if(language) {
+    getFeaturedRepos(language);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
+  
+}
 // add event listeners to forms
 userFormEl.addEventListener('submit', formSubmitHandler);
+// event lister for language buttons
+languageButtonsEl.addEventListener("click", buttonClickHandler);
